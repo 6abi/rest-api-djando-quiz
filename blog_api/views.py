ecb72e3eac9a64043183fbfe0e98ec6497d9f4db
Barbara <6abi.cardoso@gmail.com>
2021-09-02 23:40:00 -0300

    Criação do serializer para transformar os dados do banco em JSON

--- blog_api/views.py
+++ blog_api/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import generics
+from blog.models import Post
+from .serializers import PostSerializer
 
-# Create your views here.
+class PostList(generics.ListCreateAPIView):
+    """Listar o conteúdo e limpar depois """
+    queryset = Post.postobjects.all()
+    serializer_class = PostSerializer
+    pass
+
+class  PostDetail(generics.RetrieveDestroyAPIView):
+    """Detalhres de cada item do POST e limpar depois """
+    pass
