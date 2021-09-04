ac897073847b2346ee71128f43150592be92cc17
Barbara <6abi.cardoso@gmail.com>
2021-09-03 22:06:24 -0300

    Teste do POST como admin

--- blog/admin.py
+++ blog/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from . import models
 
 
-@admin.register(models.Model)
+@admin.register(models.Post)
 class AuthorAdmin(admin.ModelAdmin):
     list_display = ('title', 'id', 'status', 'slug', 'author')
     prepopulated_fields = {'slug': ('title',), }
