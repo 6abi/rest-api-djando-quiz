b87208a8b7e5ced28d51764d953176b1a3feee61
Barbara <6abi.cardoso@gmail.com>
2021-09-07 23:16:15 -0300

    permissão para visualizar o item(mesmo não sendo o autor da publicação)

--- blog_api/views.py
+++ blog_api/views.py
@@ -1,7 +1,18 @@
 from rest_framework import generics
 from blog.models import Post
 from .serializers import PostSerializer
-from rest_framework.permissions import IsAdminUser, DjangoModelPermissions
+from rest_framework.permissions import SAFE_METHODS, BasePermission, IsAdminUser, DjangoModelPermissions
+
+
+class PostUserWritePermission(BasePermission):
+    """ Permissões de Edição dos Posts, somente é editável pelo autor """
+    message = "Edições em uma publicação somente podem ser feitas pelos próprios autores"
+
+    def has_object_permission(self, request, view, obj):
+
+        if request.method in SAFE_METHODS:
+            return True
+        return obj.author == request.user
 
 class PostList(generics.ListCreateAPIView):
     """Listar o conteúdo e limpar depois """
@@ -13,7 +24,8 @@ class PostList(generics.ListCreateAPIView):
     serializer_class = PostSerializer
 
 
-class  PostDetail(generics.RetrieveDestroyAPIView):
-    """Detalhes de cada item do POST e limpar depois """
+class  PostDetail(generics.RetrieveUpdateDestroyAPIView, PostUserWritePermission):
+    """Detalhes de cada item do POST (cada JSON) e limpar depois """
+    permission_classes = [PostUserWritePermission]
     queryset =  Post.objects.all()
     serializer_class =  PostSerializer
